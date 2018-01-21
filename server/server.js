1d6d75f5761dac1023f2bbd9dd0aa70f6858ad99
George Guerrero <george.guerrero@gmail.com>
2018-01-21 16:38:53 -0600

    add GET todos:id

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+const {ObjectId} = require('mongodb');
+
 var express = require('express');
 var bodyParser = require('body-parser');
 
@@ -9,26 +11,42 @@ var app = express();
 
 app.use(bodyParser.json());
 
-app.post('/todos', function(req, res) {
+app.post('/todos', function (req, res) {
     var todo = new ToDo({
         text: req.body.text
     });
-    todo.save().then(function(doc) {
+    todo.save().then(function (doc) {
         res.send(doc);
-    }, function(e){
+    }, function (e) {
         res.status(400).send(e);
     });
 });
 
-app.get('/todos', function(req, res) {
-    ToDo.find().then(function(todos){
-    res.send({todos: todos});
+app.get('/todos', function (req, res) {
+    ToDo.find().then(function (todos) {
+        res.send({todos: todos});
     }, function (e) {
-    res.status(400).send(e);
+        res.status(400).send(e);
     });
 });
 
-app.listen(3000, function() {
+app.get('/todos/:id', function (req, res) {
+    var id = req.params.id;
+
+    if (!ObjectId.isValid(id)) {
+        return res.status(404).send();
+    }
+    ToDo.findById(id).then(function (todo) {
+        if (!todo) {
+            return res.status(404).send();
+        }
+        res.send({todo});
+    }).catch(function (e) {
+        res.status(400).send();
+    });
+});
+
+app.listen(3000, function () {
     console.log('Started on port 3000');
 });
 
