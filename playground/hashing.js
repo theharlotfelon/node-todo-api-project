c4b6d4fd5845c7744712bd717f45887fdb41ae0c
George Guerrero <george.guerrero@gmail.com>
2018-01-30 22:01:00 -0600

    hash user password before save

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,16 +1,30 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-    id: 10
-};
+var password = '123abc!';
 
+bcrypt.genSalt(10, function(err, salt) {
+    bcrypt.hash(password, salt, function(err, hash) {
+        //console.log(hash);
+    });
+});
 
+var hashedPassword = '$2a$10$xYc4pB8jLY2lPurKzbO6LeUBfVNBFtyBYCpvI4AYT02TcnVbAyb82';
 
-var token = jwt.sign(data, '123abc');
-console.log(token);
-var decoded = jwt.verify(token, '123abc');
-console.log('decoded', decoded);
+bcrypt.compare(password, hashedPassword, function(err, res) {
+    console.log(res);
+});
+// var data = {
+//     id: 10
+// };
+//
+//
+//
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
+// var decoded = jwt.verify(token, '123abc');
+// console.log('decoded', decoded);
 
 // var message = 'I am user number 3';
 // var hash = SHA256(message).toString();
