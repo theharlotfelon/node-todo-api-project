609b751652e9eb9f1ce56bdbd60362437ab480a7
George Guerrero <george.guerrero@gmail.com>
2018-02-02 00:16:32 -0600

    add POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -86,6 +86,26 @@ UserSchema.pre('save', function(next) {
     }
 });
 
+UserSchema.statics.findByCredentials = function (email, password) {
+    var User = this;
+
+    return User.findOne({email}).then(function(user) {
+        if(!user) {
+            return Promise.reject();
+        }
+
+        return new Promise(function (resolve, reject) {
+            bcrypt.compare(password, user.password, function(err, res) {
+                if (res) {
+                    resolve(user);
+                } else {
+                    reject();
+                }
+            });
+        })
+    });
+};
+
 var User = mongoose.model('User', UserSchema);
 
 module.exports = {
--- server/server.js
+++ server/server.js
@@ -110,6 +110,18 @@ app.get('/users/me', authenticate, function(req,res) {
     res.send(req.user);
 });
 
+app.post('/users/login', function (req, res) {
+    var body = _.pick(req.body, ['email', 'password']);
+
+    User.findByCredentials(body.email, body.password).then(function(user) {
+        return user.generateAuthToken().then(function(token){
+            res.header('x-auth', token).send(user);
+        })
+    }).catch(function (e) {
+        res.status(400).send();
+    })
+});
+
 module.exports = {
     app: app
 };
