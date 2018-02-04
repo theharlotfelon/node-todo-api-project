c81f2608f5f72119f82512d32295402f25a9e9dd
George Guerrero <george.guerrero@gmail.com>
2018-02-04 13:02:48 -0600

    add DELETE /users/me/token route

--- server/server.js
+++ server/server.js
@@ -80,46 +80,54 @@ app.patch('/todos/:id', function (req, res) {
     }
 
     ToDo.findByIdAndUpdate(id, {$set: body}, {new: true}).then(function (todo) {
-        if(!todo) {
+        if (!todo) {
             return res.status(404).send();
         }
         res.send({todo: todo});
-    }).catch(function(e) {
+    }).catch(function (e) {
         res.status(400).send();
-    })
+    });
 });
 
 app.listen(port, function () {
     console.log(`Started on port ${port}`);
 });
 
-app.post('/users', function(req, res) {
+app.post('/users', function (req, res) {
     var body = _.pick(req.body, ['email', 'password']);
     var user = new User(body);
 
     user.save().then(function () {
         return user.generateAuthToken();
-    }).then(function (token){
+    }).then(function (token) {
         res.header('x-auth', token).send(user);
-    }).catch(function (e){
+    }).catch(function (e) {
         res.status(400).send(e);
-    })
+    });
 });
 
-app.get('/users/me', authenticate, function(req,res) {
+app.get('/users/me', authenticate, function (req, res) {
     res.send(req.user);
 });
 
 app.post('/users/login', function (req, res) {
     var body = _.pick(req.body, ['email', 'password']);
 
-    User.findByCredentials(body.email, body.password).then(function(user) {
-        return user.generateAuthToken().then(function(token){
+    User.findByCredentials(body.email, body.password).then(function (user) {
+        return user.generateAuthToken().then(function (token) {
             res.header('x-auth', token).send(user);
-        })
+        });
     }).catch(function (e) {
         res.status(400).send();
-    })
+    });
+});
+
+app.delete('/users/me/token', authenticate, function (req, res) {
+    req.user.removeToken(req.token).then(function () {
+        res.status(200).send();
+    }, function () {
+        res.status(400).send();
+    });
 });
 
 module.exports = {
