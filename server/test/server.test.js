218e7b0ca8d9e83b9e7c9e1661ace9ecdd7c86e4
George Guerrero <george.guerrero@gmail.com>
2018-02-04 12:40:05 -0600

    add tests for POST /users/login

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -253,3 +253,55 @@ describe('POST /users', function() {
             .end(done);
     });
 });
+
+describe('POST /users/login', function() {
+    it('should login user and return auth token', function(done) {
+        request(app)
+            .post('/users/login')
+            .send({
+                email: users[1].email,
+                password: users[1].password
+            })
+            .expect(200)
+            .expect(function (res) {
+                expect(res.headers['x-auth']).toExist();
+            })
+            .end(function(err, res) {
+                if (err) {
+                    return done(err);
+                }
+                User.findById(users[1]._id).then(function(user) {
+                   expect(user.tokens[0]).toInclude({
+                       access: 'auth',
+                       token: res.headers['x-auth']
+                   });
+                   done();
+                }).catch(function (e) {
+                    done(e);
+        });
+            });
+    });
+    it('should reject invalid login', function(done) {
+        request(app)
+            .post('/users/login')
+            .send({
+                email: users[1].email,
+                password: 'password321'
+            })
+            .expect(400)
+            .expect(function(res) {
+                expect(res.headers['x-auth']).toNotExist();
+            })
+            .end(function(err, res) {
+                if (err) {
+                    return done(err);
+                }
+                User.findById(users[1]._id).then(function(user) {
+                    expect(user.tokens.length).toBe(0);
+                    done();
+                }).catch(function(e) {
+                    done(e);
+                });
+            });
+    });
+});
